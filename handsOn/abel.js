a0392cf32b82cf8aa465d72fba02ad2926bfc894
Abel <abelsena1@hotmail.com>
2019-07-29 08:25:44 -0300

    FOI FEITA AS ALTERAÇÕES REFERENTE O NOME DAS VARIÁVEIS

--- handsOn/abel.js
+++ handsOn/abel.js
@@ -11,11 +11,11 @@ const object = {
 
 // EXEMPLOS DE ARRAY - VETOR E MATRIZ NO JAVA SCRIPT
 
-const time = ['Corinthians', 'São Paulo', 'Palmeiras','Santos', 1, 2, 3, 4]
-console.log (time[1]) // MOSTRA O VALOR DO ARRAY NA POSIÇÃO INDICADA
-console.log (time.length) // MOSTRA O TAMANHO DO ARRAY
-console.log (time) // LISTA TODOS OS VALORES DO ARRAY
-console.table(time) // MOSTRA OS VALORES DO ARRAY EM TABELA 
+const teams = ['Corinthians', 'São Paulo', 'Palmeiras','Santos', 1, 2, 3, 4]
+console.log (teams[1]) // MOSTRA O VALOR DO ARRAY NA POSIÇÃO INDICADA
+console.log (teams.length) // MOSTRA O TAMANHO DO ARRAY
+console.log (teams) // LISTA TODOS OS VALORES DO ARRAY
+console.table(teams) // MOSTRA OS VALORES DO ARRAY EM TABELA 
 
 // ARRAY COM OBJETOS
 
@@ -27,42 +27,43 @@ console.table (arrayOfObjects)  // MOSTRA OS VALORES DO ARRAY EM TABELA
 
 // FUNÇÕES DE ARRAY'S
 
-const corinthians = ['Cassio', 'Fagner', 'Jadson', 'Pedrinho','Ramiro']
-console.log (corinthians)
-corinthians.pop() // REMOVE O ULTIMO ELEMENTO DO ARRAY
-console.log (corinthians)
+const players_corinthians = ['Cassio', 'Fagner', 'Jadson', 'Pedrinho','Ramiro']
+console.log (players_corinthians)
+players_corinthians.pop() // REMOVE O ULTIMO ELEMENTO DO ARRAY
+console.log (players_corinthians)
 
-corinthians.push('Vagner Love') // ADICIONA NA ULTIMA POSIÇÃO DO ARRAY
-console.log (corinthians)
+players_corinthians.push('Vagner Love') // ADICIONA NA ULTIMA POSIÇÃO DO ARRAY
+console.log (players_corinthians)
 
-corinthians.shift() // REMOVE A PRIMEIRA POSIÇÃO
-console.log (corinthians)
+players_corinthians.shift() // REMOVE A PRIMEIRA POSIÇÃO
+console.log (players_corinthians)
 
-corinthians.unshift('Walter') // ADICIONA NA PRIMEIRA POSIÇÃO DO ARRAY
-console.log (corinthians)
+players_corinthians.unshift('Walter') // ADICIONA NA PRIMEIRA POSIÇÃO DO ARRAY
+console.log (players_corinthians)
 
 // METODO SPLICE PODE ADICIONAR QUANTO REMOVER ELEMENTOS DENTRO DE UM ARRAY
 
-corinthians.splice (2, 0, 'Danilo Avelar', 'Lucas Pinton') /* AQUI ESTOU ADICIONANDO APARTIR DO ÍNDICE 02
+players_corinthians.splice (2, 0, 'Danilo Avelar', 'Lucas Pinton') /* AQUI ESTOU ADICIONANDO APARTIR DO ÍNDICE 02
 E NÃO REMOVENDO */
-console.log (corinthians)
+console.log (players_corinthians)
 
-corinthians.splice (3, 1) // REMOVENDO APENAS 01 ELEMENTO NA 3ª POSIÇÃO
-console.log (corinthians)
+players_corinthians.splice (3, 1) // REMOVENDO APENAS 01 ELEMENTO NA 3ª POSIÇÃO
+console.log (players_corinthians)
 
 // METODO SLICE SERVE PARA RETORNAR UM NOVO ARRAY
 
-const jogadores = corinthians.slice(3) // VAI SEPARAR APENAS OS ARRAYS APARTIR DO ÍNDICE 03
-console.log (jogadores)
+const players = players_corinthians.slice(3) // VAI SEPARAR APENAS OS ARRAYS APARTIR DO ÍNDICE 03
+console.log (players)
 
 // OUTRA FORMA DE ARRAY COM OBJETOS
-const jogador_sp = {0: 'Jean', 1: 'Luan', 2: 'Liziero'}
-console.log(jogador_sp)
-Object.defineProperty(jogador_sp, 'toString', {
+const players_sp = {0: 'Jean', 1: 'Luan', 2: 'Liziero'}
+console.log(players_sp)
+Object.defineProperty(players_sp, 'toString', {
     value: function () { return Object.value (this)}, 
     enumerable: false
 })
-console.log (jogador_sp [2])
+console.log (players_sp [2])
+
 
 
 
