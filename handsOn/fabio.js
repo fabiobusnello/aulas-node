58fd40cbbb5bbbed0e0efacbd649bb9148267af9
fabiobusnello <fabiocouto22@gmail.com>
2019-08-02 19:40:12 -0300

    termino da aula de arrays

--- handsOn/fabio.js
+++ handsOn/fabio.js
@@ -1,22 +1,22 @@
 // Objetos js
-const object = {
-  "key-teste": "value",
-  keyString: "valueString",
-  keyInt: 1
-}
+// const object = {
+//   "key-teste": "value",
+//   keyString: "valueString",
+//   keyInt: 1
+// }
 
-// Matrizes/array
-const array = ["Abel", "Fábio", "Magno", "Maze", 1, 2]
+// // Matrizes/array
+// const array = ["Abel", "Fábio", "Magno", "Maze", 1, 2]
 
-const arrayOfObjects = [{name: "Fabio"}, {name: "Magno"}, {name: "Abel"}, {name: "Mazé"}]
-const arraySimpleFiltered = value => value == "Magno"
-const arrayOfObjectFiltered = obj => obj.name == "Magno"
+// const arrayOfObjects = [{name: "Fabio"}, {name: "Magno"}, {name: "Abel"}, {name: "Mazé"}]
+// const arraySimpleFiltered = value => value == "Magno"
+// const arrayOfObjectFiltered = obj => obj.name == "Magno"
 
-const newDataFiltered = array.filter(arraySimpleFiltered)
-const newDataObjFiltered = arrayOfObjects.filter(arrayOfObjectFiltered)
+// const newDataFiltered = array.filter(arraySimpleFiltered)
+// const newDataObjFiltered = arrayOfObjects.filter(arrayOfObjectFiltered)
 
-console.log(newDataFiltered)
-console.log(newDataObjFiltered)
+// console.log(newDataFiltered)
+// console.log(newDataObjFiltered)
 
 // for (), .forEach(), .map(), .filter(), .reduce()
 
@@ -28,3 +28,69 @@ console.log(newDataObjFiltered)
 //         console.log("eu " + array[i] + "não sou o Magno")
 //     }
 // }
+
+// const arrayOfObjects = [
+//   { name: "Fabio" },
+//   { name: "Magno" },
+//   { name: "Abel" },
+//   { name: "Mazé" }
+// ];
+
+// let names = [];
+// const iterations = object => {
+//   names.push(object.name);
+// };
+
+// arrayOfObjects.forEach(iterations);
+
+// console.log(arrayOfObjects);
+// console.log(names);
+
+
+// const arrayOfObjects = [
+//   { name: "Fabio" },
+//   { name: "Magno" },
+//   { name: "Abel" },
+//   { name: "Mazé" },
+//   { name: 1}
+// ];
+
+// const filtered = arrayOfObjects.filter( (object, index, arrayOriginal) => index === 4)
+
+// console.log(filtered)
+
+// const arrayOfObjects = [
+//   { name: "Fabio" },
+//   { name: "Magno"},
+//   { name: "Abel" },
+//   { name: "Mazé" },
+//   { name: 1}
+// ]
+
+// const arrayOfObjectsMap = (object, index, arrayOfObjects) => {
+//   if (object.name === 'Magno') {
+//     object.lastName = 'Ozzyr'
+//   }
+//   return object
+// }
+
+// const newArrayOfObjects = arrayOfObjects.map(arrayOfObjectsMap)
+
+// console.log(newArrayOfObjects)
+
+const arrayOfObjects = [
+  { name: "Fabio" },
+  { name: "Magno"},
+  { name: 1},
+  { name: "Abel" },
+  { name: "Mazé" }
+]
+ 
+const arrayOfObjectsReduce = (accumulator, object, index, arrayOriginal) => {
+  accumulator[object.name] = 10 + index
+  return accumulator
+}
+
+const newArrayOfObjectsReducered = arrayOfObjects.reduce(arrayOfObjectsReduce, {})
+
+console.log(newArrayOfObjectsReducered)
